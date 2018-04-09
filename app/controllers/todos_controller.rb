87ec5221faad69597f4e7c113a5f060639bb40f6
Sylvance <kerandisylvance@gmail.com>
2018-04-09 17:06:48 +0300

    Add authentication routes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,13 +3,15 @@ class TodosController < ApplicationController
   
     # GET /todos
     def index
-      @todos = Todo.all
+      # get current user todos
+      @todos = current_user.todos
       json_response(@todos)
     end
   
     # POST /todos
     def create
-      @todo = Todo.create!(todo_params)
+      # create todos belonging to current user
+      @todo = current_user.todos.create!(todo_params)
       json_response(@todo, :created)
     end
   
@@ -31,10 +33,9 @@ class TodosController < ApplicationController
     end
   
     private
-  
+
     def todo_params
-      # whitelist params
-      params.permit(:title, :created_by)
+      params.permit(:title)
     end
   
     def set_todo
